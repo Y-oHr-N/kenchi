bb1683892331c7d7cb2646158b271183ccac5dc0
Y-oHr-N <kon.y.ohr.n@gmail.com>
2017-08-23 05:40:54 +0900

    update predict method

--- kenchi/base.py
+++ kenchi/base.py
@@ -11,18 +11,18 @@ class BaseDetector(BaseEstimator, metaclass=ABCMeta):
 
     @abstractmethod
     def compute_anomaly_score(self, X):
-        """Computes the anomaly score."""
+        """Compute the anomaly score."""
 
         pass
 
     @abstractmethod
     def fit(self, X, y=None):
-        """Fits the model according to the given training data."""
+        """Fit the model according to the given training data."""
 
         pass
 
     def fit_predict(self, X, y=None):
-        """"Fits the model to the training set X and returns the labels (0
+        """"Fit the model to the training set X and returns the labels (0
         inlier, 1 outlier) on the training set.
 
         Parameters
@@ -33,13 +33,13 @@ class BaseDetector(BaseEstimator, metaclass=ABCMeta):
         Returns
         -------
         is_outlier : ndarray, shape = (n_samples)
-            Returns 0 for inliers and 1 for outliers.
+            Return 0 for inliers and 1 for outliers.
         """
 
         return self.fit(X, y).predict(X)
 
     def predict(self, X):
-        """Predicts if a particular sample is an outlier or not.
+        """Predict if a particular sample is an outlier or not.
 
         Parameters
         ----------
@@ -49,21 +49,27 @@ class BaseDetector(BaseEstimator, metaclass=ABCMeta):
         Returns
         -------
         is_outlier : ndarray, shape = (n_samples)
-            Returns 0 for inliers and 1 for outliers.
+            Return 0 for inliers and 1 for outliers.
         """
 
         X = check_array(X)
 
-        return (
-            self.compute_anomaly_score(X) > self._threshold
-        ).astype(np.int32)
+        if isinstance(self._threshold, float):
+            return (
+                self.compute_anomaly_score(X) > self._threshold
+            ).astype(np.int32)
+
+        else:
+            return np.any(
+                self.compute_anomaly_score(X) > self._threshold, axis=1
+            ).astype(np.int32)
 
 
 class DetectorMixin:
     """Mixin class for all detectors."""
 
     def score(self, X, y):
-        """Returns the F1 score.
+        """Return the F1 score.
 
         Parameters
         ----------
@@ -81,14 +87,14 @@ class DetectorMixin:
 
         X, y           = check_X_y(X, y)
 
-        # Computes the confusion matrix
+        # Compute the confusion matrix
         cnf_matrix     = confusion_matrix(y, self.predict(X), [1, 0])
         tp, fn, fp, tn = np.ravel(cnf_matrix)
 
-        # Computes the specificity (a.k.a. normal sample accuracy)
+        # Compute the specificity (a.k.a. normal sample accuracy)
         r0             = tn / (fp + tn)
 
-        # Computes the sensitivity (a.k.a. anomalous sample accuracy)
+        # Compute the sensitivity (a.k.a. anomalous sample accuracy)
         r1             = tp / (tp + fn)
 
         return 2.0 * r0 * r1 / (r0 + r1)
