3d0ea407295d3a74975c958938f61cf117ffd5c5
Y-oHr-N <kon.y.ohr.n@gmail.com>
2017-08-26 00:18:36 +0900

    update __init__.py

--- kenchi/__init__.py
+++ kenchi/__init__.py
@@ -3,4 +3,4 @@ from .gaussian_distribution import GaussianDetector, GGMDetector
 from .empirical_distribution import EmpiricalDetector
 from .vmf_distribution import VMFDetector
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
