331712d7c8c055c9d3b372c78c96fa517ae5c470
Y-oHr-N <kon.y.ohr.n@gmail.com>
2017-09-03 04:32:28 +0900

    update __version__

--- kenchi/__init__.py
+++ kenchi/__init__.py
@@ -4,4 +4,4 @@ from .empirical_distribution import EmpiricalDetector
 from .mixture_distribution import GaussianMixtureDetector
 from .vmf_distribution import VMFDetector
 
-__version__ = '0.1.1'
+__version__ = '0.2.0'
