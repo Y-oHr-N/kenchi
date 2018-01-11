46b3266bbee6bc343985ad29bc73c6fb49bdee9f
Y-oHr-N <kon.y.ohr.n@gmail.com>
2018-01-12 03:12:28 +0900

    update datasets

--- kenchi/datasets.py
+++ kenchi/datasets.py
@@ -1,28 +1,36 @@
+from typing import Union
+
 import numpy as np
-from sklearn import datasets
+from sklearn.datasets import make_blobs as sklearn_make_blobs
 from sklearn.utils import check_random_state
+
+from .utils import Limits, OneDimArray, RandomState, TwoDimArray
 
 
 def make_blobs(
-    n_inliers=90,    n_outliers=10,
-    n_features=2,    centers=3,
-    cluster_std=1.0, center_box=(-10, 10),
-    shuffle=True,    random_state=None
-):
+    n_inliers:    int                       = 490,
+    n_outliers:   int                       = 10,
+    n_features:   int                       = 25,
+    centers:      Union[int,   TwoDimArray] = 5,
+    cluster_std:  Union[float, OneDimArray] = 1.,
+    center_box:   Limits                    = (-10., 10.),
+    shuffle:      bool                      = True,
+    random_state: RandomState               = None
+) -> Union[TwoDimArray, OneDimArray]:
     """Generate isotropic Gaussian blobs with outliers.
 
     Parameters
     ----------
-    n_inliers : int, default 90
+    n_inliers : int, default 490
         Number of inliers.
 
     n_outliers : int, default 10
         Number of outliers.
 
-    n_features : int, default 2
+    n_features : int, default 25
         Number of features for each sample.
 
-    centers : int or array-like of shape (n_centers, n_features), default 3
+    centers : int or array-like of shape (n_centers, n_features), default 5
         Number of centers to generate, or the fixed center locations.
 
     cluster_std : float or array-like of shape (n_centers,), default 1.0
@@ -36,21 +44,30 @@ def make_blobs(
         If True, shuffle samples.
 
     random_state : int, RandomState instance, default None
-        Seed of the pseudo random number generator to use when shuffling the
-        data.
+        Seed of the pseudo random number generator.
 
     Returns
     -------
     X : ndarray of shape (n_inliers + n_outliers, n_features)
-        Generated samples.
+        Generated data.
 
     y : ndarray of shape (n_inliers + n_outliers,)
-        Return 0 for inliers and 1 for outliers.
+        Return 1 for inliers and -1 for outliers.
+
+    References
+    ----------
+    H.-P. Kriegel, M. Schubert and A. Zimek,
+    "Angle-based outlier detection in high-dimensional data,"
+    In Proceedings of SIGKDD'08, pp. 444-452, 2008.
+
+    M. Sugiyama, and K. Borgwardt,
+    "Rapid distance-based outlier detection via sampling,"
+    Advances in NIPS'13, pp. 467-475, 2013.
     """
 
     generator        = check_random_state(random_state)
 
-    X_inliers, _     = datasets.make_blobs(
+    X_inliers, _     = sklearn_make_blobs(
         n_samples    = n_inliers,
         n_features   = n_features,
         centers      = centers,
@@ -60,13 +77,12 @@ def make_blobs(
     )
 
     X_outliers       = generator.uniform(
-        low          = center_box[0],
-        high         = center_box[1],
+        low          = np.min(X_inliers),
+        high         = np.max(X_inliers),
         size         = (n_outliers, n_features)
     )
 
     X                = np.concatenate([X_inliers, X_outliers])
-
     y                = np.empty(n_inliers + n_outliers, dtype=np.int64)
     y[:n_inliers]    = 1
     y[n_inliers:]    = -1
--- kenchi/datasets/__init__.py
+++
@@ -1,5 +0,0 @@
-from .sample_generator import make_blobs
-
-__all__ = [
-    'make_blobs'
-]
