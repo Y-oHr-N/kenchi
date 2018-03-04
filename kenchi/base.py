9a8357570863c8d8efb36ff522e257a652aea7c7
Y-oHr-N <kon.y.ohr.n@gmail.com>
2018-03-04 12:15:10 +0900

    add OutlierMixin

--- kenchi/base.py
+++ kenchi/base.py
@@ -2,11 +2,11 @@ from abc import abstractmethod, ABC
 
 import numpy as np
 from sklearn.base import BaseEstimator
-from sklearn.utils.validation import check_is_fitted, check_X_y
+from sklearn.utils.validation import check_is_fitted
 
 from .visualization import plot_anomaly_score, plot_roc_curve
 
-__all__ = ['is_outlier_detector', 'BaseOutlierDetector']
+__all__ = ['is_outlier_detector', 'BaseOutlierDetector', 'OutlierMixin']
 
 
 def is_outlier_detector(estimator):
@@ -26,13 +26,36 @@ def is_outlier_detector(estimator):
     return getattr(estimator, '_estimator_type', None) == 'outlier_detector'
 
 
-class BaseOutlierDetector(BaseEstimator, ABC):
+class OutlierMixin:
+    """Mixin class for all outlier detectors."""
+
+    _estimator_type = 'outlier_detector'
+
+    def fit_predict(self, X, y=None):
+        """Fit the model according to the given training data and predict if a
+        particular sample is an outlier or not.
+
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            Training Data.
+
+        y : ignored
+
+        Returns
+        -------
+        y_pred : array-like of shape (n_samples,)
+            Return 1 for inliers and -1 for outliers.
+        """
+
+        return self.fit(X).predict(X)
+
+
+class BaseOutlierDetector(BaseEstimator, OutlierMixin, ABC):
     """Base class for all outlier detectors in kenchi."""
 
     # TODO: Update anomaly_score method so that the normalized score can be
     # computed
-
-    _estimator_type = 'outlier_detector'
 
     @abstractmethod
     def __init__(self, contamination=0.01, verbose=False):
@@ -40,7 +63,7 @@ class BaseOutlierDetector(BaseEstimator, ABC):
         self.verbose       = verbose
 
     @abstractmethod
-    def fit(self, X, y=None, **fit_params):
+    def fit(self, X, y=None):
         """Fit the model according to the given training data.
 
         Parameters
@@ -110,28 +133,6 @@ class BaseOutlierDetector(BaseEstimator, ABC):
             threshold = self.threshold_
 
         return np.where(self.anomaly_score(X) <= threshold, 1, -1)
-
-    def fit_predict(self, X, y=None, threshold=None, **fit_params):
-        """Fit the model according to the given training data and predict if a
-        particular sample is an outlier or not.
-
-        Parameters
-        ----------
-        X : array-like of shape (n_samples, n_features)
-            Training Data.
-
-        y : ignored
-
-        threshold : float, default None
-            User-provided threshold.
-
-        Returns
-        -------
-        y_pred : array-like of shape (n_samples,)
-            Return 1 for inliers and -1 for outliers.
-        """
-
-        return self.fit(X, **fit_params).predict(X, threshold=threshold)
 
     def plot_anomaly_score(self, X, **kwargs):
         """Plot the anomaly score for each sample.
@@ -230,7 +231,5 @@ class BaseOutlierDetector(BaseEstimator, ABC):
         ax : matplotlib Axes
             Axes on which the plot was drawn.
         """
-
-        X, y = check_X_y(X, y)
 
         return plot_roc_curve(y, self.anomaly_score(X), **kwargs)
