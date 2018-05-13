254534aa97a43af1bc48dc62c1a5bb61ba8db68a
Y-oHr-N <kon.y.ohr.n@gmail.com>
2018-05-13 19:36:23 +0900

    update PipelineTest class

--- kenchi/tests/test_pipeline.py
+++ kenchi/tests/test_pipeline.py
@@ -2,13 +2,12 @@ import doctest
 import unittest
 
 import matplotlib
-import matplotlib.axes
-from sklearn.exceptions import NotFittedError
-from sklearn.preprocessing import StandardScaler
-
 from kenchi import pipeline
 from kenchi.datasets import make_blobs
 from kenchi.outlier_detection import SparseStructureLearning
+from matplotlib.axes import Axes
+from sklearn.exceptions import NotFittedError
+from sklearn.preprocessing import StandardScaler
 
 matplotlib.use('Agg')
 
@@ -23,44 +22,80 @@ def load_tests(loader, tests, ignore):
 
 class PipelineTest(unittest.TestCase):
     def setUp(self):
-        self.X, self.y = make_blobs(centers=1, random_state=1)
-        self.sut       = pipeline.Pipeline([
-            ('standardize', StandardScaler()),
-            ('detect',      SparseStructureLearning(assume_centered=True))
-        ])
         _, self.ax     = plt.subplots()
+        self.X, self.y = make_blobs(centers=1, random_state=0)
+        self.sut       = pipeline.Pipeline([
+            ('scaler', StandardScaler()),
+            ('det', SparseStructureLearning(assume_centered=True))
+        ])
 
     def tearDown(self):
         plt.close()
 
-    def test_anomaly_score_notfitted(self):
-        with self.assertRaises(NotFittedError):
-            self.sut.anomaly_score(self.X)
+    def test_score_samples(self):
+        self.sut.fit(self.X)
 
-    def test_featurewise_anomaly_score_notfitted(self):
-        with self.assertRaises(NotFittedError):
-            self.sut.featurewise_anomaly_score(self.X)
+        score_samples = self.sut.score_samples(self.X)
+
+        self.assertEqual(self.y.shape, score_samples.shape)
+
+    def test_anomaly_score(self):
+        self.sut.fit(self.X)
+
+        anomaly_score = self.sut.anomaly_score(self.X)
+
+        self.assertEqual(self.y.shape, anomaly_score.shape)
+
+    def test_featurewise_anomaly_score(self):
+        self.sut.fit(self.X)
+
+        anomaly_score = self.sut.featurewise_anomaly_score(self.X)
+
+        self.assertEqual(self.X.shape, anomaly_score.shape)
 
     def test_plot_anomaly_score(self):
+        self.sut.fit(self.X)
+
         self.assertIsInstance(
-            self.sut.fit(self.X).plot_anomaly_score(self.X, ax=self.ax),
-            matplotlib.axes.Axes
+            self.sut.plot_anomaly_score(self.X, ax=self.ax), Axes
         )
 
     def test_plot_roc_curve(self):
+        self.sut.fit(self.X)
+
         self.assertIsInstance(
-            self.sut.fit(self.X).plot_roc_curve(self.X, self.y, ax=self.ax),
-            matplotlib.axes.Axes
+            self.sut.plot_roc_curve(self.X, self.y, ax=self.ax), Axes
         )
 
     def test_plot_graphical_model(self):
-        self.assertIsInstance(
-            self.sut.fit(self.X).plot_graphical_model(ax=self.ax),
-            matplotlib.axes.Axes
-        )
+        self.sut.fit(self.X)
+        self.assertIsInstance(self.sut.plot_graphical_model(ax=self.ax), Axes)
 
     def test_plot_partial_corrcoef(self):
-        self.assertIsInstance(
-            self.sut.fit(self.X).plot_partial_corrcoef(ax=self.ax),
-            matplotlib.axes.Axes
+        self.sut.fit(self.X)
+        self.assertIsInstance(self.sut.plot_partial_corrcoef(ax=self.ax), Axes)
+
+    def test_score_samples_notfitted(self):
+        self.assertRaises(NotFittedError, self.sut.score_samples)
+
+    def test_anomaly_score_notfitted(self):
+        self.assertRaises(NotFittedError, self.sut.anomaly_score)
+
+    def test_featurewise_anomaly_score_notfitted(self):
+        self.assertRaises(
+            NotFittedError, self.sut.featurewise_anomaly_score, self.X
         )
+
+    def test_plot_anomaly_score_notfitted(self):
+        self.assertRaises(NotFittedError, self.sut.plot_anomaly_score)
+
+    def test_plot_roc_curve_notfitted(self):
+        self.assertRaises(
+            NotFittedError, self.sut.plot_roc_curve, self.X, self.y
+        )
+
+    def test_plot_graphical_model_notfitted(self):
+        self.assertRaises(NotFittedError, self.sut.plot_graphical_model)
+
+    def test_plot_partial_corrcoef_notfitted(self):
+        self.assertRaises(NotFittedError, self.sut.plot_partial_corrcoef)
