fa46704b029dd9bd6b5412e7249b85c659230ed1
Y-oHr-N <kon.y.ohr.n@gmail.com>
2018-06-23 19:15:39 +0900

    add docstring

--- kenchi/utils.py
+++ kenchi/utils.py
@@ -1,4 +1,6 @@
 def check_contamination(contamination, low=0., high=0.5):
+    """Raise ValueError if the contamination is not valid."""
+
     if not low < contamination <= high:
         raise ValueError(
             f'contamination must be in (low, high] but was {contamination}'
