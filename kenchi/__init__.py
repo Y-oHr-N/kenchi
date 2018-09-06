970efa7eeae4f15e0709fe495481011a789eef15
Y-oHr-N <kon.y.ohr.n@gmail.com>
2018-09-07 02:09:50 +0900

    add __KENCHI_SETUP__ variable

--- kenchi/__init__.py
+++ kenchi/__init__.py
@@ -1,8 +1,14 @@
-from . import datasets # noqa
-from . import metrics # noqa
-from . import outlier_detection # noqa
-from . import pipeline # noqa
-from . import plotting # noqa
-from . import utils # noqa
+__version__          = '0.9.0'
 
-__version__ = '0.9.0'
+try:
+    __KENCHI_SETUP__
+except NameError:
+    __KENCHI_SETUP__ = False
+
+if not __KENCHI_SETUP__:
+    from . import datasets # noqa
+    from . import metrics # noqa
+    from . import outlier_detection # noqa
+    from . import pipeline # noqa
+    from . import plotting # noqa
+    from . import utils # noqa
--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+import builtins
 from setuptools import setup
+
+builtins.__KENCHI_SETUP__ = True
 
 setup()
