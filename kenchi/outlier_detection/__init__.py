cae6b49480fdb4ccd43e6408a76039f24768bb8e
Y-oHr-N <kon.y.ohr.n@gmail.com>
2018-09-10 00:47:29 +0900

    update __init__.py

--- kenchi/outlier_detection/__init__.py
+++ kenchi/outlier_detection/__init__.py
@@ -1,8 +1,23 @@
-from .angle_based import *  # noqa
-from .classification_based import *  # noqa
-from .clustering_based import *  # noqa
-from .density_based import *  # noqa
-from .distance_based import *  # noqa
-from .ensemble import *  # noqa
-from .reconstruction_based import *  # noqa
-from .statistical import *  # noqa
+from .angle_based import FastABOD
+from .classification_based import OCSVM
+from .clustering_based import MiniBatchKMeans
+from .density_based import LOF
+from .distance_based import KNN, OneTimeSampling
+from .ensemble import IForest
+from .reconstruction_based import PCA
+from .statistical import GMM, HBOS, KDE, SparseStructureLearning
+
+DETECTORS = dict(
+    fast_abod=FastABOD(),
+    oc_svm=OCSVM(),
+    mini_batch_k_means=MiniBatchKMeans(),
+    lof=LOF(),
+    knn=KNN(),
+    one_time_sampling=OneTimeSampling(),
+    i_forest=IForest(),
+    pca=PCA(),
+    gmm=GMM(),
+    hbos=HBOS(),
+    kde=KDE(),
+    sparse_structure_learning=SparseStructureLearning(),
+)
