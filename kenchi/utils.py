055964d3a33b554c4994d19bd77483efba104bfe
Y-oHr-N <kon.y.ohr.n@gmail.com>
2018-09-30 23:00:17 +0900

    fix IForest class

--- kenchi/utils.py
+++ kenchi/utils.py
@@ -1,7 +1,7 @@
 def check_contamination(contamination, low=0., high=0.5):
     """Raise ValueError if the contamination is not valid."""
 
-    if not low < contamination <= high:
+    if contamination != 'auto' and not low < contamination <= high:
         raise ValueError(
             f'contamination must be in (low, high] but was {contamination}'
         )
